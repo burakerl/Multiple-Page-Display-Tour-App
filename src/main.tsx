acbfb51ec796cbbfd0e26341d10d1dd45161fd09
burakerl <burakerol.j@gmail.com>
2026-03-03 14:49:23 +0300

    Fix: Clean up main.tsx and routing setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,6 @@ import { createRoot } from "react-dom/client";
 import Header from "./components/Header.tsx";
 import { HashRouter } from "react-router-dom";
 
-/*import AboutUs from "./components/AboutUs";
-import NotFoundPage from "./components/NotFoundPage.tsx";
-import Services from "./components/Services.tsx";
-import Blog from "./components/Blog.tsx";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/about-us", element: <AboutUs /> },
-  { path: "*", element: <NotFoundPage /> },
-  { path: "/services", element: <Services /> },
-  { path: "/blog", element: <Blog /> },
-]); */
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
